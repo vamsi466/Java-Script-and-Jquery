d5525ea7208e8ebca152730b3fd2535e5ad6c6bf
vamsi466 <ramsumanth730@gmail.com>
2016-11-22 13:52:45 +0530

    Add files via upload

--- Game/JS/game.js
+++ Game/JS/game.js
@@ -1,22 +1,55 @@
+
+
+//function to decide the game winner or is it a draw match
  function gameDecision(){
 
+  //considering all the blocks on the game board
 	var blocks = $(".activeblock");
+
+  //array in which each game block with text appende is injected
 	var textinblocks = [];
-	for(var i = 0; i<blocks.length;i++){
+
+  //a loop for having the blocks with text into it after having a click on the block
+  var blocklength = blocks.length;
+	for(var i = 0; i<blocklength;i++){
 		textinblocks.push($(blocks[i]).text());
 	}
 
-	if((textinblocks[0] == "X" && textinblocks[1] == "X" &&  textinblocks[2] == "X") || (textinblocks[3] == "X" && textinblocks[4] == "X" &&  textinblocks[5] == "X") || (textinblocks[6] == "X" && textinblocks[7] == "X" &&  textinblocks[8] == "X") || (textinblocks[0] == "X" && textinblocks[3] == "X" &&  textinblocks[6] == "X") || (textinblocks[1] == "X" && textinblocks[4] == "X" &&  textinblocks[7] == "X") || (textinblocks[2] == "X" && textinblocks[5] == "X" &&  textinblocks[8] == "X") || (textinblocks[0] == "X" && textinblocks[4] == "X" &&  textinblocks[8] == "X") ||(textinblocks[2] == "X" && textinblocks[4] == "X" &&  textinblocks[6]   == "X" )){
 
+  //having a condition to declare which player is winner or a draw match
+  //checking the condition whether we are having 3 continuos blocks of player1 which is with text "X"
+  //if any player have 3 blocks of his/her text appended horizontally / vertically/ diagonally continuosally he is the winner
+  //player1 winning condition
+
+	if((textinblocks[0] == "X" && textinblocks[1] == "X" &&  textinblocks[2] == "X") ||
+  (textinblocks[3] == "X" && textinblocks[4] == "X" &&  textinblocks[5] == "X") ||
+  (textinblocks[6] == "X" && textinblocks[7] == "X" &&  textinblocks[8] == "X") ||
+  (textinblocks[0] == "X" && textinblocks[3] == "X" &&  textinblocks[6] == "X") ||
+  (textinblocks[1] == "X" && textinblocks[4] == "X" &&  textinblocks[7] == "X") ||
+  (textinblocks[2] == "X" && textinblocks[5] == "X" &&  textinblocks[8] == "X") ||
+  (textinblocks[0] == "X" && textinblocks[4] == "X" &&  textinblocks[8] == "X") ||
+  (textinblocks[2] == "X" && textinblocks[4] == "X" &&  textinblocks[6]   == "X" )){
 		return "PlayerOne";
 
 	}
 
-	else if((textinblocks[0] == "O" && textinblocks[1] == "O" &&  textinblocks[2] == "O") || (textinblocks[3] == "O" && textinblocks[4] == "O" &&  textinblocks[5] == "O") || (textinblocks[6] == "O" && textinblocks[7] == "O" &&  textinblocks[8] == "O") || (textinblocks[0] == "O" && textinblocks[3] == "O" &&  textinblocks[6] == "O") || (textinblocks[1] == "O" && textinblocks[4] == "O" &&  textinblocks[7] == "O") || (textinblocks[2] == "O" && textinblocks[5] == "O" &&  textinblocks[8] == "O") || (textinblocks[0] == "O" && textinblocks[4] == "O" &&  textinblocks[8] == "O") ||(textinblocks[2] == "O" && textinblocks[4] == "O" &&  textinblocks[6] == "O")    ){
+  //player2 winning condition
+
+	else if((textinblocks[0] == "O" && textinblocks[1] == "O" &&  textinblocks[2] == "O") ||
+  (textinblocks[3] == "O" && textinblocks[4] == "O" &&  textinblocks[5] == "O") ||
+  (textinblocks[6] == "O" && textinblocks[7] == "O" &&  textinblocks[8] == "O") ||
+  (textinblocks[0] == "O" && textinblocks[3] == "O" &&  textinblocks[6] == "O") ||
+  (textinblocks[1] == "O" && textinblocks[4] == "O" &&  textinblocks[7] == "O") ||
+  (textinblocks[2] == "O" && textinblocks[5] == "O" &&  textinblocks[8] == "O") ||
+  (textinblocks[0] == "O" && textinblocks[4] == "O" &&  textinblocks[8] == "O") ||
+  (textinblocks[2] == "O" && textinblocks[4] == "O" &&  textinblocks[6] == "O")){
 
 		return "PlayerTwo"
 
 	}
+
+  //if none of the blocks are remained empty and no player won the match then it declare as draw
+  //if any block remianed untouched/clicked then the game is still continued
 
 	if((textinblocks.indexOf("clickme")) != -1){
 
@@ -32,50 +65,74 @@
 
 
 
-
+//the functionality is ready
  $(document).ready(function(){
+
+   //when a block is clicked then its event is taken and based on that change that with respect to player which player is active
+   //.one is used so that no block can,t be clicked more thqn once
    $(".activeblock").one('click',function(eventstarted){
+
+     //.target gives which block is current target
      var activatedBlock = eventstarted.target;
 
+
+     //to see which player is active either player1 or 2
      var firstPlayer = $(".player1").hasClass("activeplayer");
      var secondPlayer = $(".player2").hasClass("activeplayer");
 
 
-
+     //if first player is active then the respective changes are made to that block
      if(firstPlayer && !secondPlayer){
 
+
+       //we are activating second player and deactivating first player after first player click
        $(".player1").removeClass("activeplayer");
        $(".player2").addClass("activeplayer");
+
+       //changing the color or the block with respect first player color
        $(activatedBlock).css("background-color","lightblue");
+
+       //appending the text to that div/block with the letter "X"
        $(activatedBlock).text("X");
 
      }
 
+     //if second player is active then the respective changes are made to that block
      if(!firstPlayer && secondPlayer){
 
+       //we are activating first player and deactivating second player after second player click
        $(".player1").addClass("activeplayer");
        $(".player2").removeClass("activeplayer");
+
+       //changing the color or the block with respect second player color
        $(activatedBlock).css("background-color","blue");
+
+        //appending the text to that div/block with the letter "O"
        $(activatedBlock).text("O");
 
-     }
+     }//this will be continued until all the blocks are targeted once
 
 
-    
 
+    // calling a function to decide who is the winner of the game
 	  var result = gameDecision();
 
+    //declaring the winner based on the output of function
 		if(result != "resume"){
 			if(result != "draw"){
 
+        //throwing an alert about the winner
 				alert(result+" is the Winner...");
 
 
 			}else{
 
+        //if none of them win then declaring the match as DRAW
 				alert("Match Draw");
 
 			}
+
+      //reloading the game after the game is completed with a setTimeout of 2 seconds
 			setTimeout(function(){location.reload()},2000);
 
 		}
--- JSONtoTable(mobileresponsive)/JS/jsontotable.js
+++ JSONtoTable(mobileresponsive)/JS/jsontotable.js
@@ -1,32 +1,64 @@
 $(document).ready(function(){
+
+	//starting the functionality
+	//considering an empty array of employeedetails
 	var EmployeeDetails = [];
 		console.log(EmployeeDetails);
+
+		//doing an ajax call to get the ionformation/data of the employees which is there in json format
+		//if the ajax call has a success output we get an array of output
 		$.ajax({url: "employeedetails.json", success: function(result){
 			console.log(result)
+
+			//getting the array through success of ajax call we store it in EmployeeDetails[]
 			EmployeeDetails = result;
 			console.log(EmployeeDetails);
+
+		//appending the table to the div with id detailstoshow
 		$("#detailstoshow").append($('<table class="table table-hover"></table>'))
+
+		//storing that appended div in a variable tab
 		var tab = $("#detailstoshow").children()[0];
+
+		//now appending tablebody as <tbody> to that tab
 		$(tab).append($("<tbody></tbody>"));
+
+		//now that child of tab is stored in variable table
 		var table = $(tab).children()[0];
+
+		//appending the row to the table as a child to it
 		$(table).append($("<tr></tr>"));
+
+		//the child is stored in a variable named header
 		var header = $(table).children()[0];
 
+		//now iterarting the first row and getting the index by key value pairs
+		//now appending that key values as the header to each column
 			for(var index in EmployeeDetails[0]){
 				$(header).append($("<th>"+index+"</th>"));
 			}
-			//appending array elements to DOM
 
-			for(var i=0;i<EmployeeDetails.length;i++){
+
+
+			//now iterating the employees in the EmployeeDetails[] to get all the employee details
+			var employeeslength = EmployeeDetails.length;
+			for(var i=0;i<employeeslength;i++){
+
+				//declaring a row
 				var row = $("<tr></tr>")
+
+				//appending all the values to that row
 				  $(row).append($("<td>"+EmployeeDetails[i].ID+"</td>"));
 					$(row).append($("<td>"+EmployeeDetails[i].Name+"</td>"));
 					$(row).append($("<td>"+EmployeeDetails[i].Designation+"</td>"));
 					$(row).append($("<td>"+EmployeeDetails[i].Experience+"</td>"));
 					$(row).append($("<td>"+EmployeeDetails[i].Salary+"</td>"));
+
+					//now appending that row to the table after the header is done
 				$(table).append(row);
 			}
-			//function to check for repetion
+			//appending all the employee 
+
 
 		}});
 });
